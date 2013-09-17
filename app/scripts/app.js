5b43763236881f7a4da269cb2297b428969df95f
Ban Calets <bancalets@gmail.com>
2013-09-17 11:36:45 +0200

    inital commit to src with a lot of stuff

---
+++ app/scripts/app.js
@@ -0,0 +1,13 @@
+'use strict';
+
+angular.module('HelloDollyApp', [])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
